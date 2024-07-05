4fd8da5c25f18ff14fc6d56c127ce01a73618593
Omar Rwemi <omsharp@gmail.com>
2024-07-05 14:23:15 +0200

    use auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,33 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany();
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <div>{image.name}</div>
+          <img src={image.url}></img>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="w-48">
-            <div>{image.name}</div>
-            <img src={image.url}></img>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
